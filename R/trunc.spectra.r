ffdc1b9e82f536615423d2bdbe63d30b01548514
Basil <damselfly@yandex.ru>
2017-12-27 14:12:47 +0300

    Added trunc.spectra()

---
+++ R/trunc.spectra.r
@@ -0,0 +1,27 @@
+#' A function to truncate anomalous "horns" from multifrcatal spectra
+#' @param mf spectra object returned by local.spectra()
+
+trunc.spectra <- function(mf)
+{
+  n <- ncol(mf$f)
+  len <- nrow(mf$f)
+  start <- which(mf$q == 0)
+  for (ii in 1:n)
+  {
+    pos <- start - 1
+    while (mf$f[pos, ii] <= mf$f[pos+1, ii] & pos != 1) pos <- pos - 1
+    if (pos != 1) 
+    {
+      mf$f[1:pos, ii] <- NA
+      mf$alfa[1:pos, ii] <- NA
+    }
+    pos <- start + 1
+    while (mf$f[pos, ii] <= mf$f[pos-1, ii] & pos != len) pos <- pos + 1
+    if (pos != len) 
+    {
+      mf$f[pos:len, ii] <- NA
+      mf$alfa[pos:len, ii] <- NA
+    }
+  }
+  return(mf)
+}
