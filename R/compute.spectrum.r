09f9915f4dd2cd233c2ea9e3752523c33a0d49ce
basil-yakimov <damselfly@yandex.ru>
2018-12-20 11:09:12 +0300

    add weihting support for compute.spectrum

--- R/compute.spectrum.r
+++ R/compute.spectrum.r
@@ -1,18 +1,20 @@
 #' A function to compute global multifractal spectrum
 #' @param mom moments object returned by compute.moments.lin()
+#' @param weigh whether to weigh points with scale or not
 
-compute.spectrum <- function(mom)
+compute.spectrum <- function(mom, weigh = F)
 {
   n <- length(mom$a)
-  tau <- pp <- delta <- Dq <- rep(0,length(mom$q))
+  tau <- pp <- delta <- Dq <- rep(0, length(mom$q))
   xx <- log(mom$a)
+  if (weigh) w <- xx - min(xx) + 1 else w <- NULL
   for (jj in 1:length(mom$q))
   {
     yy <- log(mom$qD[,jj])
     fin <- is.finite(yy)
     
-    fit.lin <- lm(yy[fin] ~ xx[fin])
-    fit.quad <- lm(yy[fin] ~ xx[fin] + I(xx[fin]^2))
+    fit.lin <- lm(yy[fin] ~ xx[fin], weights = w)
+    fit.quad <- lm(yy[fin] ~ xx[fin] + I(xx[fin]^2), weights = w)
     
     pp[jj] <- anova(fit.quad)[2,5]
     delta[jj] <- AIC(fit.quad) + (2*4*5/(n-4-1)) - AIC(fit.lin) - (2*3*4/(n-3-1))
@@ -24,7 +26,7 @@ compute.spectrum <- function(mom)
 
   alfa <- tau
   f <- tau
-  alfa <- -derv(tau,mom$q[2]-mom$q[1])
+  alfa <- -derv(tau, mom$q[2] - mom$q[1])
   f <- mom$q*alfa + tau
   
   return(list(q = mom$q, tau = tau, Dq = Dq, alfa = alfa, f = f, pp = pp, delta = delta))
