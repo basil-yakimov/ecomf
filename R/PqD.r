2ce04906cc9dafbf490b13282b87b9b9e5a39e56
Василий Якимов <damselfly@yandex.ru>
2020-11-26 11:57:34 +0300

    add

--- R/PqD.r
+++ R/PqD.r
@@ -1,6 +1,7 @@
 #' A function to compute phylogenetic diversity indices
 #' @param dat matrix of abundances
 #' @param tree phylogenetic tree
+#' @param dist functional distance matrix
 #' @param q vector of orders
 #' @param hill transformation to phylogenetic hill numbers
 
@@ -47,6 +48,50 @@ PqD <- function(dat, tree, q = 0:2, hill = T)
   }
   
   out <- t(apply(dat, 1, PqD.line))
+  
+  if (length(q) == 1) out <- as.vector(out)
+  
+  return(out)
+}
+
+FqD <- function(dat, dist, q = 0:2, hill = T)
+{
+  dist <- dist[colnames(dat), colnames(dat)]
+  
+  FqD.line <- function(x)
+  {
+    x <- x/sum(x)
+    dd <- dist[x > 0, x > 0]
+    x <- x[x > 0]
+    rao <- x %*% dd %*% x
+    temp <- x %*% t(x)
+    
+    qD <- rep(0, length(q))
+    
+    if (length(x) == 1) return(qDQ)
+    
+    for (ii in 1:length(q))
+    {
+      qD[ii] <- ( (x^q[ii] %*% dd %*% x^q[ii]) / rao^q[ii] ) ^ (1/(1-q[ii]))
+    }
+    
+    qD[q == 1] <- exp(- sum(dd * temp/rao[1] * log(temp/rao[1])))
+    
+    if (hill) qD <- sqrt(qD/rao)
+    
+    return(qD)
+  }
+  
+  dat <- as.matrix(dat)
+  nr <- nrow(dat)
+  nc <- ncol(dat)
+  if (nc == 1) {
+    dat <- t(dat)
+    nr <- nrow(dat)
+    nc <- ncol(dat)
+  }
+  
+  out <- t(apply(dat, 1, FqD.line))
   
   if (length(q) == 1) out <- as.vector(out)
   
